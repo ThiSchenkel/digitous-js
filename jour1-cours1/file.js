25c5d26cca0f590b0e899f7f89056112a51c94d7
ThiSchenkel <thi.schenkel@gmail.com>
2020-12-07 14:18:36 +0100

    Working on it

---
+++ jour1-cours1/file.js
@@ -0,0 +1,18 @@
+// 01- Hello World
+console.log("Hello World !");
+
+// 02- My name is Thi
+var test = "My name is Thi";
+console.log(test);
+
+// 03- Nice to meet you Thi
+var name = "Thi";
+console.log("Nice to meet you " + name);
+
+// 04- 15
+var testLength = "I'm very long !";
+console.log(testLength.length);
+
+// 05- 
+var food = "croissant is meh";
+console.log(food.replace("meh", "so good"));
