6e882139a3abef6af7dcb96e7dc52381f4317117
ThiSchenkel <thi.schenkel@gmail.com>
2020-12-07 15:25:53 +0100

    Exercices finished

--- jour1-cours1/file.js
+++ jour1-cours1/file.js
@@ -13,6 +13,32 @@ console.log("Nice to meet you " + name);
 var testLength = "I'm very long !";
 console.log(testLength.length);
 
-// 05- 
+// 05- croissant is so good
 var food = "croissant is meh";
 console.log(food.replace("meh", "so good"));
+
+// 06- This is Cool THIS IS COOL this is cool
+var basic = "This is Cool";
+var basicUp = "This is Cool";
+var basicDown = "This is Cool";
+console.log(basic, basicUp.toUpperCase(), basicDown.toLowerCase());
+
+// // 07- [ 'b', 'a', 'n', 'a', 'n', 'a' ]
+var word = "banana";
+var letters = word.split;
+console.log(word.split(""));
+
+
+// 08- I'm 49 years old
+var age = 49;
+var template = "I'm 49 years old";
+console.log(template);
+
+
+
+
+
+
+
+
+
