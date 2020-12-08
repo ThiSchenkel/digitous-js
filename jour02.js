675da52317341ab5a2dbe21b3e5c3102059ac339
ThiSchenkel <thi.schenkel@gmail.com>
2020-12-08 14:54:07 +0100

    Working on it

---
+++ jour02.js
@@ -0,0 +1,72 @@
+// 01 - Number => 102 13.9
+var integer = 102;
+var float = 13.9;
+console.log(integer, float);
+
+// 02 - Convert
+var basic = 34;
+stringifield = basic.toString();
+console.log(stringifield);
+
+// 03 - Round => 2
+var num = 1.5;
+var rounded = Math.round(1.5);
+console.log(rounded);
+
+// 04 - Arithmétique => 
+var test = 12;
+var bis = 5;
+console.log(test + bis); // 17
+console.log(test - bis); // 7
+console.log(test / bis); // 2.4
+console.log(test * bis); // 60
+console.log(test ** bis); // 248832
+console.log(test % bis); // 2
+
+// 05 - Comparaison
+var test = 143;
+var bis = 219;
+console.log(test > bis); // false
+console.log(test < bis); // true
+console.log(test = bis); // 219
+console.log(test == bis); // true
+console.log(test === bis); // true
+console.log(test >= bis); // true
+console.log(test <= bis); // true
+
+// 06 - Condition
+var limit = 50;
+var score = 64;
+if (score >= limit) {
+    console.log("Ok good!");
+} else {
+    console.log("On nooooo....!");
+}
+
+// 07 - Condition 2
+var password = "azerty";
+if (password.length > 5) {
+    console.log("The password is secure");
+}
+
+// 08 - Condition
+var password = "azerty";
+var passwordSecure = password.length
+if (password.length > 5) {
+    console.log("The password is secure")
+} else if (password = password.length) {
+    console.log("Everything is good");
+} else if (password < password.length) {
+    console.log("Something is good");
+} else {
+    console.log("Nothing is good");
+}
+
+
+
+
+
+
+
+
+
