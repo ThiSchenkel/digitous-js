1d719303d5089de355c19b430c3057273b39edaa
ThiSchenkel <thi.schenkel@gmail.com>
2020-12-14 15:35:59 +0100

    Day4, working on it exo.4 done

--- jour04.js
+++ jour04.js
@@ -40,21 +40,42 @@
 // - Créez une fonction `checkIfEven` qui reçoit un paramètre `num`
 // - Dans la fonction, à l'aide d'une condition, affichez "even" si `num` est pair, "odd" si il est impair
 // - Appelez votre fonction plusieurs fois avec des chiffres différents comme argument pour vérifier les résultats (penser au modulo)
-function checkIfEven(num) {
-    if (num % 2 == 0) {
-        num = num++, "Even";
-        console.log("Even", num);
-        return num;
-    }
-    else {
-        console.log("Odd", num);
-        return num;
+// function checkIfEven(num) {
+//     if (num % 2 == 0) {
+//         num = num++, "Even";
+//         console.log("Even", num);
+//         return num;
+//     }
+//     else {
+//         console.log("Odd", num);
+//         return num;
+//     }
+// }
+// var myNum = checkIfEven(16);
+// console.log(myNum);
+
+// 04 - Compare
+// - Créez une fonction `compare` qui reçoit deux paramètres `num1` et `num2`
+// - Dans la fonction, à l'aide d'une condition, affichez selon les cas :
+//     - "`num1` is bigger" si `num1` est plus grand
+//     - "`num2` is bigger" si `num2` est plus grand
+//     - "both are the same" si ils sont égaux
+// - Appelez votre fonction plusieurs fois avec des chiffres différents pour vérifier les résultats
+function compare(num1, num2) {
+    if (num1 > num2) {
+        console.log("Num1 is bigger");
+        return num1;
+    } else if
+        (num2 > num1) {
+        console.log("Num2 is bigger");
+        return num2;
+    } else if
+        (num1 === num2) {
+        console.log("Both are the same");
     }
 }
-var myNum = checkIfEven(13);
-console.log(myNum);
-
-
+var myCompare = compare(7, 7);
+console.log(myCompare);
 
 
 
