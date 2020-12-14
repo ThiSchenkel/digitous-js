f24d93650378ddb232753e4038bc6bfff53ee3e1
ThiSchenkel <thi.schenkel@gmail.com>
2020-12-14 14:56:28 +0100

    Day4, working on it exo.3 done

--- jour04.js
+++ jour04.js
@@ -19,18 +19,43 @@
 // - Créez une variable `cats` qui contient un tableau contenant les variables `cat` et `cat2`
 // - Affichez la valeur de `age` de `cat` dans la console
 // - Affichez la valeur de `isCute` de `cat2` dans la console
-var cat2 = {
-    name: "Taki",
-    age: 10,
-    isCute: false,
-}
+// var cat2 = {
+//     name: "Taki",
+//     age: 10,
+//     isCute: false,
+// }
 
-var cats = {
-    name: "Garfield",
-    age: 3,
-    isCute: true,
-    name: "Taki",
-    age: 10,
-    isCute: false,
+// var cats = {
+//     name: "Garfield",
+//     age: 3,
+//     isCute: true,
+//     name: "Taki",
+//     age: 10,
+//     isCute: false,
+// }
+// console.log(cats.age);
+// console.log(cat2.isCute);
+
+//  03 - Even
+// - Créez une fonction `checkIfEven` qui reçoit un paramètre `num`
+// - Dans la fonction, à l'aide d'une condition, affichez "even" si `num` est pair, "odd" si il est impair
+// - Appelez votre fonction plusieurs fois avec des chiffres différents comme argument pour vérifier les résultats (penser au modulo)
+function checkIfEven(num) {
+    if (num % 2 == 0) {
+        num = num++, "Even";
+        console.log("Even", num);
+        return num;
+    }
+    else {
+        console.log("Odd", num);
+        return num;
+    }
 }
-console.log(cats.age, cat2.isCute);
+var myNum = checkIfEven(13);
+console.log(myNum);
+
+
+
+
+
+
