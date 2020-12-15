85abb9573e0d137e1c6de696f80892792b5f57b7
ThiSchenkel <thi.schenkel@gmail.com>
2020-12-15 17:28:20 +0100

    Stille working on it

--- jour05-exoX.js
+++ jour05-exoX.js
@@ -1,37 +1,40 @@
+var { addition } = require("./table-utils.js");
+
+
 // 01 - Calculator
 // - Créez une fonction `calculate` qui recevra trois paramètres (deux nombres entiers et un opérateur `+`, `-`, `x`, `/` ou `%`)
 // - Créez une condition (méthode au choix) pour chaque valeur possible de l'opérateur qui retournera le résultat du calcul
 // - Affichez le résultat dans la console (vérifiez que pour les arguments `5`, `*` et `4` vous obtenez bien `20`)
 // - Vous devrez donner les arguments dans le terminal en appelant le programme alors attention aux paramètres !
 //     ⇒ Si vous ne donnez pas trois arguments, le programme doit afficher "error" dans la console
-// function calculate(num1, operator, num2) {
-//     var number1 = parseInt(num1);
-//     var number2 = parseInt(num2);
-//     if (operator === "+") {
-//         console.log("Vous avez fait une addition");
-//         return number1 + number2;
-//     }
-//     if (operator === "-") {
-//         console.log("Vous avez fait une soustraction");
-//         return number1 - number2;
-//     }
-//     if (operator === "x") {
-//         console.log("Vous avez fait une muntiplication");
-//         return number1 * number2;
-//     }
-//     if (operator === "/") {
-//         console.log("Vous avez fait une division");
-//         return number1 / number2;
-//     }
-//     if (operator === "%") {
-//         console.log("Vous avez fait un pourcentage");
-//         return number1 % number2;
-//     }
-//     else {
-//         console.log("Error");
-//     }
-// }
-// console.log(calculate(process.argv[2], process.argv[3], process.argv[4]));
+function calculate(num1, operator, num2) {
+    var number1 = parseInt(num1);
+    var number2 = parseInt(num2);
+    if (operator === "+") {
+        console.log("Vous avez fait une addition");
+        return number1 + number2;
+    }
+    if (operator === "-") {
+        console.log("Vous avez fait une soustraction");
+        return number1 - number2;
+    }
+    if (operator === "x") {
+        console.log("Vous avez fait une muntiplication");
+        return number1 * number2;
+    }
+    if (operator === "/") {
+        console.log("Vous avez fait une division");
+        return number1 / number2;
+    }
+    if (operator === "%") {
+        console.log("Vous avez fait un pourcentage");
+        return number1 % number2;
+    }
+    else {
+        console.log("Error");
+    }
+}
+console.log(calculate(process.argv[2], process.argv[3], process.argv[4]));
 
 
 // 02 - Table
@@ -40,12 +43,24 @@
 // - Vous devrez donner l'argument dans le terminal en appelant le programme
 //     ⇒ Si vous ne donnez pas d'argument, le programme doit afficher "error" dans la console
 function multiply(num) {
+    var table = 0;
     for (i = 1; i <= 10; i++) {
+        table = (i + "x" + num + " = " + num * i);
         console.log(i + "x" + num + " = " + num * i);
     }
-    return num;
+    return table;
 }
 console.log(multiply(process.argv[2]));
+
+
+// 03 - Separate Table
+// - Créez un fichier `table-utils.js`
+// - Mettez votre fonction `multiply` dans ce fichier, et ajouter en une deuxieme `addition` qui reprend le même principe mais avec une addition au lieu d'une multiplication
+// - Exportez les deux fonctions graces aux modules, et importez les dans votre fichier précedant pour refaire marcher votre code
+// - Affichez la table d'addition
+console.log(addition(process.argv[2]));
+
+
 
 
 
---
+++ table-utils.js
@@ -0,0 +1,26 @@
+function multiply(num) {
+    var table = 0;
+    for (i = 1; i <= 10; i++) {
+        table = (i + "x" + num + " = " + num * i);
+        console.log(i + "x" + num + " = " + num * i);
+    }
+    return table;
+}
+multiply(5);
+
+
+function addition(num) {
+    var somme = 0;
+    for (i = 1; i <= 10; i++) {
+        somme = i + num;
+        console.log(i + "+" + num + " = " + somme);
+    }
+    return somme;
+}
+addition(2);
+
+
+module.exports = {
+    multiply,
+    addition,
+};
