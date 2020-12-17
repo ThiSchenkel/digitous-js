cf7ac93cbb7bf3b93c0f900882086724aa3ef759
ThiSchenkel <thi.schenkel@gmail.com>
2020-12-17 13:17:12 +0100

    Day7 brain works slowly today

--- jour07.js
+++ jour07.js
@@ -1,13 +1,18 @@
 // 01 - Alphabet
 // - Créer une fonction `sortLetters` qui reçoit une string en paramètre
 // - La fonction retourne une string avec les mêmes lettres, dans l'ordre alphabétique
-// - Ne pas utilisez la méthode .sort()
 // - Appelez votre fonction avec "konexio" comme argument et vérifiez que vous obtenez "eiknoox"
 function sortLetters(sortLettersString) {
-    var sortLettersString = "konexio";
+    var sortLettersString = (sortLettersString);
     var sortLettersSplit = sortLettersString.split("");
     var sortLettersSort = sortLettersSplit.sort(); {
         console.log(sortLettersSort.toString());
     }
 }
-sortLetters();
+sortLetters("konexio");
+
+// 02 - XOXO
+// - Créez une fonction `countEach` qui reçoit une string contenant des x et des o en paramètre
+// - La fonction retourne true si il y a autant de x que de o, sinon elle retourne false
+// - Vérifiez que l'argument "xxxoooox" déclenche un true, et que "xox" déclenche un false
+
