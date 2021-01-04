796cde242a07cb805c91cdd31c24694bfa8b4467
ThiSchenkel <thi.schenkel@gmail.com>
2021-01-04 15:24:04 +0100

    suite exo

--- jour07.js
+++ jour07.js
@@ -19,3 +19,42 @@ var double = array.map(function (num) {
     return num * 2;
 });
 console.log(double);
+
+
+// 03 - Map Names - A TERMINER
+// - Créez une variable `longNames` 
+// - Créez une variable `shortNames` qui appellera la méthode `.map()` pour contenir une version compacte de `longNames` :
+// Affichez les valeurs de shortNames dans la console
+
+var longNames = [
+    {
+        firstName: "Jane",
+        lastName: "Doe"
+    },
+    {
+        firstName: "John",
+        lastName: "Smith"
+    }
+]
+
+var shortNames = longNames.map(function (element) {
+    var name = element.firstName + element.lastName;
+    return name;
+});
+
+console.log(shortNames);
+
+
+// 04 - Filter Numbers
+// - Créez une variable `array` contenant un tableau qui contiendra les valeurs `1`, `"toto"`, `34`, `"javascript"` et `8`
+// - Créez une variable `numbers` qui appellera la méthode `.filter()` pour contenir les **nombres** de `array`
+// - Affichez les valeurs de `numbers` dans la console
+
+var array = [1, "toto", 34, "javascript", 8];
+var number = array.filter(function (number) {
+    return number >= 0;
+});
+console.log(number);
+
+
+
