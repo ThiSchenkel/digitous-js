42ad37edff62d8657bddba063de1e67f8177a08a
ThiSchenkel <thi.schenkel@gmail.com>
2021-01-04 18:30:51 +0100

    exo presque fini (le 3 à revoir) et bonus à faire

--- jour07.js
+++ jour07.js
@@ -65,6 +65,53 @@ var even = numbers.filter(function (element) {
 console.log(even);
 
 
+// 06 - Cakes
+// - Créez une variable `cakes` contenant la valeur suivante 
+// - À l'aide des méthodes `.filter()` et `.map()`, vous donnerez le statut "sold out !" aux gâteaux au chocolat
+// - Ces deux gâteaux doivent être stockés dans une nouvelle variable, affichez-la dans la console
+var cakes =
+    [
+        {
+            name: "cake",
+            flavor: "vanilla",
+            status: "available"
+        },
+        {
+            name: "brownie",
+            flavor: "chocolate",
+            status: "available"
+        },
+        {
+            name: "pie",
+            flavor: "strawberry",
+            status: "available"
+        },
+        {
+            name: "muffin",
+            flavor: "pistachio",
+            status: "available"
+        },
+        {
+            name: "donut",
+            flavor: "chocolate",
+            status: "available"
+        },
+    ]
+
+var soldOut = cakes.filter(function (cake) {
+    if (cake.flavor == "chocolate") {
+        return true;
+    } else {
+        return false;
+    }
+}).map(function (cake) {
+    if (cake.flavor == "chocolate") {
+        cake.status = "Sold out!";
+    }
+    return cake;
+});
+console.table(soldOut);
+
 
 
 
