6155ff8a5733fd53ff0ffad00ab563893f605deb
ThiSchenkel <thi.schenkel@gmail.com>
2021-01-04 14:08:03 +0100

    File systeme

--- jour07.js
+++ jour07.js
@@ -7,3 +7,15 @@ var fs = require("fs");
 fs.readFile("./jour07.txt", function (error, data) {
     console.log(data.toString());
 })
+
+
+// 02 - Map Double
+// - Créez une variable `array` contenant un tableau qui contiendra les valeurs `1`, `2`, `3`, `4` et `5`
+// - Créez une variable `double` qui appellera la méthode `.map()` pour contenir les doubles des valeurs de `array`
+// - Affichez les valeurs de `double` dans la console
+
+var array = [`1`, `2`, `3`, `4`, `5`];
+var double = array.map(function (num) {
+    return num * 2;
+});
+console.log(double);
