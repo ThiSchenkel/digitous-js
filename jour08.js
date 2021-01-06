0cb077a9258c9600e9e21ed2041928270bcac08c
ThiSchenkel <thi.schenkel@gmail.com>
2021-01-06 11:37:53 +0100

    correction done

--- jour08.js
+++ jour08.js
@@ -16,6 +16,16 @@ function getCountries(allCountries) {
 }
 getCountries();
 
+// avec .map()
+request.get("https://restcountries.eu/rest/v1/all", function (error, response, body) {
+    var countries = JSON.parse(body);
+    var pays = countries.map(function (element) {
+        return element.name;
+    })
+    console.log(pays.join(" - "));
+})
+
+
 
 //02 - Chuck Norris
 // À l'aide de l'api suivante (lisez bien la documentation) : https://api.chucknorris.io/
@@ -24,7 +34,7 @@ getCountries();
 function getFact(blagounette) {
     request.get("https://api.chucknorris.io/jokes/random", function (error, response, body) {
         var blague = JSON.parse(body);
-        console.log(blague);
+        console.log("Blague de Chuck Norris :", blague.value);
     });
 };
 getFact();
@@ -34,7 +44,10 @@ getFact();
 // 03 - Pokemon
 // À l'aide de la superbe [PokeAPI](https://pokeapi.co/) (bien lire la documentation) :
 // - Créez une fonction asynchrone `catchPokemon` qui recevra un nombre en paramètre (il correspondra à l'id d'un pokemon) et qui récupèrera le pokemon correspondant à cet id. Elle affichera ensuite dans la console : id: xxx // remplacer xxx par l'id du pokemon - name: "xxx" // remplacer xxx par le nom du pokémon
-request.get(" https://pokeapi.co/api/v2/pokemon/ditto", function (error, response, body) {
-    var pokedex = JSON.parse(body);
-    console.log(pokedex);
-});
+function catchPokemon(id) {
+    request.get(`https://pokeapi.co/api/v2/pokemon/${id}`, function (error, response, body) {
+        var pokemon = JSON.parse(body);
+        console.log(`Mon pokemon numéro ${pokemon.id} s'appelle : ${pokemon.name}`);
+    })
+};
+catchPokemon(125);
